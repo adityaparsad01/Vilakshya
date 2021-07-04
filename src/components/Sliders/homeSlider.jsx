0f1ae3ea98f683f6fb532bb930ba2353d8e14a93
adityaparsad01 <adityaparsad01@gmail.com>
2021-07-05 05:14:01 +0530

    js

--- src/components/Sliders/homeSlider.jsx
+++ src/components/Sliders/homeSlider.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {FaArrowAltCircleLeft,FaArrowAltCircleRight} from 'react-icons/fa'
 
 import {SliderData} from './SliderData'
@@ -6,18 +6,35 @@ import useStyles from './styles';
 
 const HomeSlider = ()=>{
   const classes = useStyles();
+  const [current,setCurrent]=useState(0)
+  const length = SliderData.lenght
+  
+  const nextSlide = () => {
+    setCurrent(current === length -1 ? 0 : current + 1)
+  }
+  
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length -1 : current -1)
+  }
+  
+  if (!Array.isArray(SliderData) || SliderData.lenght <= 0) {
+    return null
+  }
+  
   return (
     <section className={classes.slide}>
-    <FaArrowAltCircleLeft className={classes.leftArrow}/>
-    <FaArrowAltCircleRight className={classes.rightArrow}/>
+    <FaArrowAltCircleLeft className={classes.leftArrow} onClick={prevSlide}/>
+    <FaArrowAltCircleRight className={classes.rightArrow} onClick={nextSlide}/>
     {
       SliderData.map((slides,index)=>((
-          <img src={slides.image} alt='img' className={classes.image}/>
+        <div className={index === current ? 'active slide ' : 'slide'} key={index}>
+        {index === current && (
+        <img src={slides.image} alt='img' className={classes.image}/>
+        )}
+        </div>
       )))
     }
-    
     </section>
     )
 }
-
 export default HomeSlider
