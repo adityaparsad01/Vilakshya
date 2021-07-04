23797455d1645614f3eb84b17dcc125f60fca9a9
adityaparsad01 <adityaparsad01@gmail.com>
2021-07-05 04:13:06 +0530

    icons

--- src/components/Sliders/homeSlider.jsx
+++ src/components/Sliders/homeSlider.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import {FaArrowAltCircleLeft,FaArrowAltCircleRight} from 'react-icons/fa'
 
 import {SliderData} from './SliderData'
+import useStyles from './Styles';
 
 const HomeSlider = ()=>{
+  const classes = useStyles();
   return (
-    <>
+    <section className={classes.slide}>
+    <FaArrowAltCircleLeft className={classes.leftArrow}/>
+    <FaArrowAltCircleRight className={classes.rightArrow}/>
     {
       SliderData.map((slides,index)=>((
           <img src={slides.image} alt='img'/>
       )))
     }
-    </>
+    
+    </section>
     )
 }
 
