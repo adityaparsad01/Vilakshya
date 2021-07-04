e83abae9db4cc6c29fb372822a8b590ae6b19cfb
aditya <m7503345712@gmail.com>
2021-07-05 03:32:17 +0530

    added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './style.css';
 
 import Products from './components/Products/Products';
 import Navbar from './components/Navbar/Navbar';
+import HomeSlider from './components/Sliders/homeSlider'
 
 const App = () => {
   return (
     <>
     <Navbar/>
-      <Products />
+    <HomeSlider/>
+    <Products />
     </>
   );
 };
---
+++ src/components/Sliders/homeSlider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const HomeSlider = ()=>{
+  return (
+    <>
+    <div>hi</div>
+    </>
+    )
+}
+
+export default HomeSlider
